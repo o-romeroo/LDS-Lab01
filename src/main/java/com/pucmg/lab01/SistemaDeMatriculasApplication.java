ebea7de5553fcf8b2dbb49fbcc2d1c323d7869c3
Lucas Randazzo <lucasrandazzo2@gmail.com>
2024-08-25 19:02:14 -0300

    subindo projeto spring

---
+++ src/main/java/com/pucmg/lab01/SistemaDeMatriculasApplication.java
@@ -0,0 +1,13 @@
+package com.pucmg.lab01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeMatriculasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeMatriculasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pucmg/lab01/SistemaDeMatriculasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pucmg.lab01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeMatriculasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
